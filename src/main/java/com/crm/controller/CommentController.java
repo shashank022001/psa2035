8e0df8820b6e89c410aefae23b00e0284a46a71a
shashank022001 <shashankshrivastava342@gmail.com>
2024-11-28 21:56:08 +0530

    Update CommentController.java

--- src/main/java/com/crm/controller/CommentController.java
+++ src/main/java/com/crm/controller/CommentController.java
@@ -24,6 +24,8 @@ public class CommentController {
             ){
         System.out.println(1000);
         System.out.println(1000);
+         System.out.println(1000);
+        System.out.println(1000);
         Post post = postRepository.findById(postId).get();
         comment.setPost(post);
 
